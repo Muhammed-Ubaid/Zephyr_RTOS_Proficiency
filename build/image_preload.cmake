80cd49ae6e17838e29627f81551309be422040b8
Muhammed-Ubaid <59333976+Muhammed-Ubaid@users.noreply.github.com>
2024-09-25 09:13:31 +0530

    adding sensor model for clap sensor

---
+++ build/image_preload.cmake
@@ -0,0 +1,8 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf52840dk_nrf52840" CACHE INTERNAL "NCS child image controlled")
+set(BOARD_ROOT "c:/embedded_projects/zephyr_rtos_proficiency;C:/ncs/v2.6.0/nrf;C:/ncs/v2.6.0/zephyr" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "c:/Embedded_Projects/Zephyr_RTOS_Proficiency/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "c:/Embedded_Projects/Zephyr_RTOS_Proficiency/nrf52840dk_nrf52840.overlay" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "C:/ncs/toolchains/cf2149caf2/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
---
+++ drivers/sensor/GPIO_Sensor_KY_038/ky_038.h
@@ -0,0 +1,10 @@
+/**
+ * @brief : KY_038 clap sensor declaration file
+ * @author : Mohammed Ubaid
+*/
+
+#ifndef KY_038
+#define KY_038
+
+
+#endif
